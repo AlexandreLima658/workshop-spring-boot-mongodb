cebdce9faed70e6c3e6d585f86df246149e3081c
Alexandre <= alexandrec@alu.ufc.br>
2025-05-25 15:10:33 -0300

    consulta simples com @Query

--- src/main/java/com/alexandre/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexandre/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,14 @@ package com.alexandre.workshopmongo.repository;
 
 import com.alexandre.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{'title': {$regex: ?0, $options: 'i'}}")
+    List<Post> findTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
